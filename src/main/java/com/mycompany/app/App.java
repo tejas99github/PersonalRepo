72175d6252f8f1a236eb44a10e4a0879fa494242
tejas <bannore99tejas@gmail.com>
2023-09-25 16:16:15 +0530

    chages in java file

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to git Remote repo" );
+        System.out.println( "Hello World!" );
     }
 }
